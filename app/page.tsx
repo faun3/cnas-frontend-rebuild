26240fc4671abd8d19d6099e608bdf96c2a5a300
faun3 <antonioculcea@gmail.com>
2023-09-07 15:25:05 +0300

    add hero banner to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Wow this is amazing</h1>
-    </div>
+    <>
+      {/* hero */}
+      <div className="w-screen h-[600px] overflow-hidden z-[-1] bg-[url('/heroBanner.jpg')] bg-bottom relative">
+        <span className="text-white bottom-1/2 absolute left-[5%] font-bold text-3xl">
+          Your health is important. <br /> We protect it.
+        </span>
+      </div>
+    </>
   );
 }

38eb59d03edb419a6b55a3c57c9c3b1c6bff81b3
faun3 <antonioculcea@gmail.com>
2023-09-07 14:44:01 +0300

    finish nav links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,6 +11,61 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 
+const navLinksObject = [
+  {
+    sectionTitle: "About us",
+    subsections: [
+      "Status",
+      "Rules and Regulations",
+      "CNRebuild Leadership",
+      "Law 153 of 2017",
+    ],
+  },
+  {
+    sectionTitle: "Structure",
+    subsections: [
+      "Board of Directors",
+      "Executive Director",
+      "Advisory Board",
+      "Technical Secretariat",
+    ],
+  },
+  {
+    sectionTitle: "Public information",
+    subsections: [
+      "Budget",
+      "Contracts",
+      "Reports",
+      "Minutes",
+      "Agreements",
+      "Publications",
+      "Press Releases",
+      "News",
+      "Events",
+      "FAQs",
+      "Transparency Portal",
+    ],
+  },
+  {
+    sectionTitle: "Legislation",
+    subsections: ["In Romania", "In the EU"],
+  },
+  {
+    sectionTitle: "Contact",
+    subsections: [
+      "Contact Us",
+      "Public Relations",
+      "Hearings",
+      "Contact Form",
+      "Phone numbers",
+      "Public",
+      "Open to public",
+      "Hearings with medical service providers",
+      "Data safety",
+    ],
+  },
+];
+
 const Nav = () => {
   return (
     <nav className="flex justify-between md:max-w-[calc(100%-256px)] my-auto">
@@ -19,43 +74,24 @@ const Nav = () => {
       {/* actual nav */}
       <NavigationMenu>
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>About us</NavigationMenuTrigger>
-            <NavigationMenuContent className="flex flex-col min-w-[300px]">
-              <NavigationMenuLink>Link</NavigationMenuLink>
-
-              <NavigationMenuLink>Link</NavigationMenuLink>
-
-              <NavigationMenuLink>Link</NavigationMenuLink>
-
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Structure</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Public information</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Legislation</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+          {navLinksObject.map((navSection, idx) => {
+            return (
+              <NavigationMenuItem key={idx}>
+                <NavigationMenuTrigger>
+                  {navSection.sectionTitle}
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="flex flex-col min-w-[300px]">
+                  {navSection.subsections.map((subsection, ssIdx) => {
+                    return (
+                      <NavigationMenuLink key={ssIdx}>
+                        {subsection}
+                      </NavigationMenuLink>
+                    );
+                  })}
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            );
+          })}
         </NavigationMenuList>
       </NavigationMenu>
     </nav>

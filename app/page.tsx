f96f333eac937d723224c2f4594a8dad35bce44f
faun3 <antonioculcea@gmail.com>
2023-09-12 16:03:37 +0300

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,24 @@ export default function Home() {
       <div className="flex justify-center">
         <Hero />
       </div>
+      <div>
+        <div className="text-[2.5rem] font-bold my-[8rem] text-center px-[8rem]">
+          <div className="mb-[2rem]">
+            Think insurance is complicated? <br />
+            Think again.
+          </div>
+          <div className="text-left text-[1.5rem] font-semibold text-gray mb-[2rem]">
+            We provide insurance for individuals, employers and businesses, so
+            you are covered for the unexpected. We take pride in our work,
+            providing you with the best medical services possible, all in one
+            easy to use package.
+          </div>
+          <button className="px-[1.5rem] py-[0.75rem] text-white font-semibold bg-mainBlue rounded-[10px] text-[1.5rem] hover:bg-mainBlue/80 active:bg-mainBlue/80 active:scale-90">
+            Join us
+          </button>
+        </div>
+      </div>
+
       <div className="flex justify-center mb-[2rem] mt-[2rem]">
         <Services />
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,77 +9,79 @@ import bottomLeft from "@/public/bottomLeft.jpg";
 
 const Hero = () => {
   return (
-    <div className="p-[3rem] flex flex-col-reverse md:flex-col lg:flex-row justify-center gap-[4rem] items-center w-fit">
-      <div className="flex flex-col">
-        <div className="relative h-fit w-fit mx-auto">
-          <div className="relative rounded-[20px] w-[20rem] h-[14rem] overflow-hidden mb-[2rem]">
-            <Image
-              className="object-cover"
-              src={top}
-              fill={true}
-              alt=""
-              aria-hidden="true"></Image>
-          </div>
-          <Image
-            className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
-            src={heroMedical}
-            width={64}
-            height={64}
-            alt=""
-            aria-hidden="true"
-          />
-        </div>
-        <div className="flex md:flex-row gap-[2rem] flex-col">
-          <div className="relative w-fit h-fit mx-auto md:mx-0">
-            <div className="relative rounded-[20px] w-[10.25rem] h-[14rem] overflow-hidden lg:ml-[1.5rem] mx-auto">
+    <div className="lg:min-h-screen flex justify-center">
+      <div className="p-[3rem] flex flex-col-reverse md:flex-col lg:flex-row justify-center gap-[4rem] items-center w-fit pt-0">
+        <div className="flex flex-col">
+          <div className="relative h-fit w-fit mx-auto">
+            <div className="relative rounded-[20px] w-[20rem] h-[14rem] overflow-hidden mb-[2rem]">
               <Image
                 className="object-cover"
-                src={bottomLeft}
+                src={top}
                 fill={true}
                 alt=""
                 aria-hidden="true"></Image>
             </div>
             <Image
               className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
-              src={heroHandshake}
+              src={heroMedical}
               width={64}
               height={64}
               alt=""
               aria-hidden="true"
             />
           </div>
-          <div className="relative w-fit h-fit mx-auto md:mx-0">
-            <div className="relative rounded-[20px] w-[16rem] h-[11rem] overflow-hidden mx-auto">
+          <div className="flex md:flex-row gap-[2rem] flex-col">
+            <div className="relative w-fit h-fit mx-auto md:mx-0">
+              <div className="relative rounded-[20px] w-[10.25rem] h-[14rem] overflow-hidden lg:ml-[1.5rem] mx-auto">
+                <Image
+                  className="object-cover"
+                  src={bottomLeft}
+                  fill={true}
+                  alt=""
+                  aria-hidden="true"></Image>
+              </div>
               <Image
-                className="object-cover"
-                src={bottomRight}
-                fill={true}
+                className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
+                src={heroHandshake}
+                width={64}
+                height={64}
                 alt=""
-                aria-hidden="true"></Image>
+                aria-hidden="true"
+              />
             </div>
-            <Image
-              className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2"
-              src={heroAmbulance}
-              height={64}
-              width={64}
-              alt=""
-              aria-hidden="true"
-            />
+            <div className="relative w-fit h-fit mx-auto md:mx-0">
+              <div className="relative rounded-[20px] w-[16rem] h-[11rem] overflow-hidden mx-auto">
+                <Image
+                  className="object-cover"
+                  src={bottomRight}
+                  fill={true}
+                  alt=""
+                  aria-hidden="true"></Image>
+              </div>
+              <Image
+                className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2"
+                src={heroAmbulance}
+                height={64}
+                width={64}
+                alt=""
+                aria-hidden="true"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="text-[3.5rem] md:text-[5rem] lg:text-[6rem] font-bold leading-[95%]">
-        We make <br />
-        insurance <br />
-        <span className="text-mainBlue flex gap-2">
-          easy{" "}
-          <Image
-            src={heroClipboard}
-            width={64}
-            height={64}
-            alt=""
-            aria-hidden="true"></Image>{" "}
-        </span>
+        <div className="text-[3.5rem] md:text-[5rem] lg:text-[6rem] font-bold leading-[95%]">
+          We make <br />
+          insurance <br />
+          <span className="text-mainBlue flex gap-2">
+            easy{" "}
+            <Image
+              src={heroClipboard}
+              width={64}
+              height={64}
+              alt=""
+              aria-hidden="true"></Image>{" "}
+          </span>
+        </div>
       </div>
     </div>
   );

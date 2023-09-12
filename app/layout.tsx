da4a913012ac5e5d52b290de9c9693f039801092
faun3 <antonioculcea@gmail.com>
2023-09-12 12:10:38 +0300

    bug: fix layout width too small on desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <Nav />
-        <div className="lg:w-[1120] md:w-[calc(836px-64px)] mx-auto">
+        <div className="lg:w-[1120px] md:w-[calc(836px-64px)] mx-auto">
           {children}
         </div>
       </body>
